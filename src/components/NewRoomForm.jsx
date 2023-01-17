30cb29eb90240662de1086f5052e8d63ef391fe5
AzizRakhim <azizbekrakhimjonov80@gmail.com>
2023-01-17 17:05:48 +0500

    feat: initial commit

---
+++ src/components/NewRoomForm.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class NewRoomForm extends Component {
+  render() {
+    return <div className="new-room-form">New room form</div>;
+  }
+}
+
+export default NewRoomForm;
